95d681adb69a85a00b563b5d6afbd11d11bc9ff4
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 10:24:32 -0300

    Update online.sh

--- online.sh
+++ online.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+USERS="/etc/xray-manager/users.xray"
 
-LOG="/var/log/xray/access.log"
-DB="/etc/xray-manager/users.db"
-
-echo "Usuarios online"
-echo ""
-
-while IFS=: read USER UUID
-do
-
-COUNT=$(grep $UUID $LOG | wc -l)
-
-if [ "$COUNT" -gt 0 ]; then
-echo "$USER - $COUNT conexoes"
+if [ ! -f "$USERS" ] || [ ! -s "$USERS" ]; then
+    echo "Nenhum usuário cadastrado."
+    exit 1
 fi
 
-done < $DB
+echo -e "Usuários online/offline:"
+
+# Criar arrays
+online_list=()
+offline_list=()
+
+while IFS=: read -r name pass uuid exp; do
+    if grep -q "$uuid" /var/log/xray/access.log; then
+        online_list+=("$name - Online - UUID: $uuid - Expira: $exp")
+    else
+        offline_list+=("$name - Offline - UUID: $uuid - Expira: $exp")
+    fi
+done < "$USERS"
+
+# Mostrar primeiro online, depois offline
+for u in "${online_list[@]}"; do
+    echo -e "$u"
+done
+for u in "${offline_list[@]}"; do
+    echo -e "$u"
+done
