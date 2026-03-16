237d53066ba15661dfb62cea5b24c37eb01df78a
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 05:24:59 -0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REPO="https://raw.githubusercontent.com/miau4/xray-manager-mult/main"
+
+echo "Atualizando XRAY MANAGER..."
+
+curl -o /usr/local/bin/menu $REPO/menu.sh
+curl -o /usr/local/bin/adduser $REPO/adduser.sh
+curl -o /usr/local/bin/deluser $REPO/deluser.sh
+curl -o /usr/local/bin/online $REPO/online.sh
+curl -o /usr/local/bin/rebuild $REPO/rebuild.sh
+curl -o /usr/local/bin/limit $REPO/limit.sh
+curl -o /usr/local/bin/expire $REPO/expire.sh
+curl -o /usr/local/bin/backup $REPO/backup.sh
+curl -o /usr/local/bin/checkjson $REPO/checkjson.sh
+curl -o /usr/local/bin/limit-monitor $REPO/limit-monitor.sh
+curl -o /usr/local/bin/expire-check $REPO/expire-check.sh
+
+chmod +x /usr/local/bin/*
+
+echo "Atualização concluída"
