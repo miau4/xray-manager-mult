af2e06b95102d277c6b6dd6de2d9f21b2cda9e39
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 05:06:58 -0300

    Update deluser.sh

--- deluser.sh
+++ deluser.sh
@@ -1,4 +1,17 @@
-
 #!/bin/bash
 
-echo "Remover manualmente do config.json por enquanto"
+CONFIG="/etc/xray/config.json"
+
+echo "Usuarios existentes:"
+jq -r '.inbounds[0].settings.clients[].id' $CONFIG
+
+echo ""
+read -p "Digite o UUID que deseja remover: " UUID
+
+jq --arg id "$UUID" '(.inbounds[0].settings.clients) |= map(select(.id != $id))' $CONFIG > /tmp/config.json
+
+mv /tmp/config.json $CONFIG
+
+systemctl restart xray
+
+echo "Usuario removido"
