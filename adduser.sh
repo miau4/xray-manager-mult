df4b6c55e2b8f665db5223513a9679eb912089e4
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 09:49:41 -0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,62 +1,50 @@
 #!/bin/bash
 
 CONFIG="/etc/xray/config.json"
+USERS="/etc/xray-manager/users.xray"
 KEYFILE="/etc/xray-manager/reality.key"
+
+# Criar arquivo de usuários se não existir
+[ ! -f "$USERS" ] && touch "$USERS"
 
 read -p "Nome do usuario: " user
 
 UUID=$(uuidgen)
-
 IP=$(curl -s ifconfig.me)
-
 PUBLIC=$(grep PUBLIC $KEYFILE | cut -d= -f2)
-
-SNI="[www.cloudflare.com](http://www.cloudflare.com)"
+SNI=$(jq -r '.inbounds[2].streamSettings.realitySettings.dest' $CONFIG)
 SID="6ba85179e30d4fc2"
 
-# adicionar cliente no VLESS xHTTP
-
-jq '.inbounds[1].settings.clients += [{"id":"'"$UUID"'","email":"'"$user"'"}]' $CONFIG > /tmp/config.json
+# Adicionar nos inbounds
+jq ".inbounds[1].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
+mv /tmp/config.json $CONFIG
+jq ".inbounds[2].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
+mv /tmp/config.json $CONFIG
+jq ".inbounds[3].settings.clients += [{\"id\":\"$UUID\",\"email\":\"$user\"}]" $CONFIG > /tmp/config.json
 mv /tmp/config.json $CONFIG
 
-# adicionar cliente no VLESS Reality
+# Salvar no arquivo exclusivo
+echo "$user:$UUID" >> $USERS
 
-jq '.inbounds[2].settings.clients += [{"id":"'"$UUID"'","email":"'"$user"'"}]' $CONFIG > /tmp/config.json
-mv /tmp/config.json $CONFIG
-
-echo "$user:$UUID" >> /etc/xray-manager/users.db
-
+# Reiniciar Xray
 systemctl restart xray
 
+# Mostrar links
 echo ""
-echo "USUARIO CRIADO"
-echo ""
+echo -e "Usuario criado: ${GREEN}$user${NC}"
 echo "UUID: $UUID"
 echo ""
-
-echo "LINK VLESS XHTTP"
-echo ""
+echo -e "${YELLOW}LINK VLESS XHTTP:${NC}"
 echo "vless://$UUID@$IP:443?encryption=none&type=xhttp&security=tls&path=/#${user}"
-
 echo ""
-echo "LINK VLESS REALITY"
-echo ""
+echo -e "${YELLOW}LINK VLESS REALITY:${NC}"
 echo "vless://$UUID@$IP:8443?encryption=none&security=reality&type=tcp&sni=$SNI&fp=chrome&pbk=$PUBLIC&sid=$SID&flow=xtls-rprx-vision#$user"
-
 echo ""
-echo "LINK VLESS WS"
-echo ""
+echo -e "${YELLOW}LINK VLESS WS:${NC}"
 echo "vless://$UUID@$IP:80?type=ws&path=/vless&security=none#$user"
-
 echo ""
-echo "LINK VMESS WS"
+echo -e "${YELLOW}LINK VMESS WS:${NC}"
+echo "vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"$user\",\"add\":\"$IP\",\"port\":\"8080\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"\",\"path\":\"/vmess\",\"tls\":\"\"}" | base64 -w0)"
 echo ""
-echo "vmess://$(echo -n "{"v":"2","ps":"$user","add":"$IP","port":"8080","id":"$UUID","aid":"0","net":"ws","type":"none","host":"","path":"/vmess","tls":""}" | base64 -w0)"
-
-echo ""
-echo "LINK VLESS TCP"
-echo ""
+echo -e "${YELLOW}LINK VLESS TCP:${NC}"
 echo "vless://$UUID@$IP:8880?type=tcp&security=none#$user"
-
-echo ""
-echo "LINKS GERADOS COM SUCESSO"
