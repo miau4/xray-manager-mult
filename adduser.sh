6e29a2d166891138563a31a6c455280fd911a8f8
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 05:47:37 -0300

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -14,7 +14,7 @@ PUBLIC=$(grep PUBLIC $KEYFILE | cut -d= -f2)
 SNI="www.cloudflare.com"
 SID="6ba85179e30d4fc2"
 
-jq --arg id "$UUID" '.inbounds[0].settings.clients += [{"id":$id}]' $CONFIG > /tmp/config.json
+jq --arg id "$UUID" '.inbounds[].settings.clients += [{"id":$id}]' $CONFIG > /tmp/config.json
 mv /tmp/config.json $CONFIG
 
 echo "$user:$UUID" >> /etc/xray-manager/users.db
@@ -29,3 +29,25 @@ echo "LINK VLESS REALITY:"
 echo ""
 
 echo "vless://$UUID@$IP:443?encryption=none&security=reality&type=tcp&sni=$SNI&fp=chrome&pbk=$PUBLIC&sid=$SID&flow=xtls-rprx-vision#$user"
+
+echo ""
+echo "LINKS GERADOS"
+echo ""
+
+echo "VLESS REALITY"
+echo "vless://$UUID@$IP:443?security=reality&type=tcp&sni=www.cloudflare.com&fp=chrome&pbk=$PUBLIC&sid=6ba85179e30d4fc2&flow=xtls-rprx-vision#$user"
+
+echo ""
+
+echo "VLESS WS"
+echo "vless://$UUID@$IP:80?type=ws&path=/vless&security=none#$user"
+
+echo ""
+
+echo "VMESS WS"
+echo "vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"$user\",\"add\":\"$IP\",\"port\":\"8080\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"\",\"path\":\"/vmess\",\"tls\":\"\"}" | base64 -w0)"
+
+echo ""
+
+echo "VLESS TCP"
+echo "vless://$UUID@$IP:8880?type=tcp&security=none#$user"
