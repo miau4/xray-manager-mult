9b8fdad8ddec5e1ecf9890887c03de12fc5f8084
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-18 19:14:47 -0300

    Update limit-monitor.sh

--- limit-monitor.sh
+++ limit-monitor.sh
@@ -1,19 +1,6 @@
+```bash
 #!/bin/bash
 
-LOG="/var/log/xray/access.log"
-LIMITDB="/etc/xray-manager/limit.db"
-
-while read line
-do
-
-USER=$(echo $line | cut -d: -f1)
-LIMIT=$(echo $line | cut -d: -f2)
-
-COUNT=$(grep $USER $LOG | wc -l)
-
-if [ "$COUNT" -gt "$LIMIT" ]; then
-echo "Usuario $USER excedeu limite"
-systemctl restart xray
-fi
-
-done < $LIMITDB
+echo "Limit monitor moderno já integrado ao limit.sh"
+exit 0
+```
