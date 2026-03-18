dfb5dca2bf2c8554f6e70cb1180a788bcfe70d47
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-18 18:14:48 -0300

    Update utils.sh

--- core/utils.sh
+++ core/utils.sh
@@ -1,1 +1,18 @@
 
+```bash
+#!/bin/bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+function msg(){
+echo -e "${GREEN}$1${NC}"
+}
+
+function error(){
+echo -e "${RED}$1${NC}"
+}
+```
