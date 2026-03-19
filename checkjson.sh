b278a7e4945b8d680719bf05ec15fb106c683465
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-19 04:35:22 -0300

    Update checkjson.sh

--- checkjson.sh
+++ checkjson.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-jq . /etc/xray/config.json > /dev/null 2>&1
+CONFIG="/etc/xray/config.json"
 
-if [ $? -eq 0 ]; then
- echo "JSON valido"
+if ! jq empty "$CONFIG" 2>/dev/null; then
+    echo "❌ JSON corrompido! Tentando restaurar..."
+
+    cp /etc/xray/config.backup.json "$CONFIG" 2>/dev/null
+
+    systemctl restart xray
+    echo "✔ Restaurado backup!"
 else
- echo "JSON quebrado"
+    echo "✔ JSON válido"
 fi
